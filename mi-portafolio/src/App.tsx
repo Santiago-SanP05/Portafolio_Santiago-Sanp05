6bbacfc3353a1a56c29b9bbd1ef1eafb9088ff22
Santiago-SanP5 <santiagopinzoncruz@gmail.com>
2026-07-31 16:43:40 -0500

    feat: :tada: Inicio de proyecto
    Se comenzó el proyecto y se realizó la estructura inicial
    
    @Santiago-SanP05

---
+++ mi-portafolio/src/App.tsx
@@ -0,0 +1,15 @@
+import Navbar from './components/navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      
+      
+    </>
+    
+  )
+}
+
+
+export default App
---
+++ mi-portafolio/src/components/navbar.tsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Inicio</a></li>
+        <li><a href="/about">Sobre mí</a></li>
+        <li><a href="/projects">Proyectos</a></li>
+        <li><a href="/contact">Contacto</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
